b792ff9f592b1d591e6cece21e6483a6e90d85ba
Kartik Verma <kartikverma90363@gmail.com>
2025-09-18 19:00:44 +0530

    middleware updated

--- middleware.js
+++ middleware.js
@@ -2,15 +2,19 @@ import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 
 import { WEBSITE_LOGIN } from "./routes/WebsiteRoute";
-import { ADMIN_DASHBOARD } from "./routes/AdminPanelRoute";
 import { USER_DASHBOARD } from "./routes/WebsiteRoute";
 
 export async function middleware(request) {
   try {
     const pathname = request.nextUrl.pathname;
+
+    if (pathname.startsWith("/api")) {
+      return NextResponse.next();
+    }
+
     const hasToken = request.cookies.has("access_token");
 
-    // 🔹 If no token
+    // If no token
     if (!hasToken) {
       // Allow auth routes without token
       if (pathname.startsWith("/auth")) {
@@ -21,22 +25,22 @@ export async function middleware(request) {
       return NextResponse.redirect(new URL(WEBSITE_LOGIN, request.nextUrl));
     }
 
-    // 🔹 Verify token
+    // Verify token
     const access_token = request.cookies.get("access_token").value;
     await jwtVerify(
       access_token,
       new TextEncoder().encode(process.env.SECRET_KEY)
     );
 
-    // ✅ If user is logged in, prevent accessing /auth/*
+    // If user is logged in, prevent accessing /auth/*
     if (pathname.startsWith("/auth")) {
       return NextResponse.redirect(new URL(USER_DASHBOARD, request.nextUrl));
     }
 
-    // ✅ Protect both admin & user dashboards
-    if (pathname.startsWith("/admin") || pathname.startsWith("/my-account")) {
-      return NextResponse.next();
-    }
+    // Protect both admin & user dashboards
+    // if (pathname.startsWith("/admin") || pathname.startsWith("/my-account")) {
+    //   return NextResponse.next();
+    // }
 
     return NextResponse.next();
   } catch (error) {
