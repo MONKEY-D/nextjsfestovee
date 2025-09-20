c9ca235adde0436e7455b2d0ae567be3c311ac3e
Kartik Verma <kartikverma90363@gmail.com>
2025-09-20 21:41:44 +0530

    commit

--- components/Website/SellerCustomerCard.jsx
+++ components/Website/SellerCustomerCard.jsx
@@ -10,7 +10,7 @@ export default function SellerCustomerCards() {
             FOR SELLERS
           </span>
           <h2 className="mt-12 sm:mt-16 text-lg sm:text-xl md:text-2xl font-bold">
-            Sell to restaurants now
+            List your products, reach verified buyers instantly.
           </h2>
           <p className="text-gray-500 mt-2 text-sm sm:text-base">
             Join 600+ sellers now
@@ -26,10 +26,10 @@ export default function SellerCustomerCards() {
             FOR CUSTOMERS
           </span>
           <h2 className="mt-12 sm:mt-16 text-lg sm:text-xl md:text-2xl font-bold text-white">
-            Smarter sourcing, better serving
+            Source directly form manufacturers at the best prices.
           </h2>
           <p className="text-rose-100 mt-2 text-sm sm:text-base">
-            Trusted by 1 lakh+ restaurants
+            Trusted by 1 lakh+ factories
           </p>
           <button className="mt-6 sm:mt-10 px-4 py-2 sm:px-6 sm:py-3 rounded-md bg-white text-black font-medium hover:bg-gray-300 transition">
             Signup now →
